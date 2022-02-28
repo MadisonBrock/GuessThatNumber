24bb8195b2b700f23f9edf17c3138b6ad9fbb7ca
Madison <madison.brock03@gmail.com>
2022-02-28 12:47:33 -0600

    Created script.js file. Completed starting variables.

---
+++ script.js
@@ -0,0 +1,13 @@
+// GUESS THAT NUMBER
+// Message to be used throughout the project/file
+const enterNumText = `Please enter a number greater then zero`; 
+// For restarting the game
+let restartGame = true
+// For storing the number that the user will guess
+let rangeNum;
+// Storing the number of attempts the user has left
+let attempts;
+// Storing the Users guess
+let guess;
+// For storing the users response to play again 
+let playAgain
