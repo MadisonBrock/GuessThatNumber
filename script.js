bd639af10c33b864c92e5c88b04020c24bd156cb
Madison <madison.brock03@gmail.com>
2022-02-28 13:50:55 -0600

    enter

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ let attempts;
 let guess;
 // For storing the users response to play again 
 let playAgain
+
+// Starting Alert message here
+alert(`Welcome to "Guess That Number!" Please click "OK" to start the game`)
+
+// Game restarts as long as the restart game variable has a value of true
+while (restartGame){
+    // Asks user to enter a number to set the upper bound for the random number that will be created
+    rangeNum = prompt(`Please enter a maximum number for the range.`)
+
+    // Using parseInt to attempt to convert the users response into a number value
+    rangeNum = parseInt (rangeNum)
+
+    // Verifies the user entry for a range number is a number greater then ZERO (NOTES: all numbers, positive or negative, have default boolean value of true, except for 0)
+    while(!rangeNum || rangeNum < 1){
+        rangeNum = prompt(enterNumText)
+        rangeNum = parseInt (rangeNum)
+    }
+
+    // Creates the random number using the range number entered by the user
+    rangeNum = Math.floor( Math.random() * rangeNum) + 1
+
+
+    break;
+}
